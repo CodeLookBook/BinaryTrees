617827e9b430bd1d72877ea53ff737fbe479fb47
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-02-11 23:38:54 +0200

    ITraversal interface was created.

---
+++ BinaryTrees/Traversal/ITraversal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees.Traversal
+{
+    public interface ITraversal <NodeT, KeyT>
+        where NodeT : class, IBinaryTreeNode<NodeT, KeyT>, IComparable<NodeT>, new()
+        where KeyT  : IComparable<KeyT>
+    {
+        void Traverse(NodeT root, Action<KeyT> action, TraversalMode mode);
+        IEnumerable<KeyT> Traverse(NodeT root, TraversalMode mode);
+    }
+}
