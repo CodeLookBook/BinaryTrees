54587eb999897472e35faf88a53c5188c8c56314
Евгений Мирошниченко <CodeLookBook@gmail.com>
2018-02-11 23:49:03 +0200

    TraversalMode Enum: Was created. Contains list of traversal modes: LeftToRight and RightToLeft.

---
+++ BinaryTrees/Traversal/TraversalMode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees.Traversal
+{
+    public enum TraversalMode
+    {
+        /// <summary>
+        /// Moves through tree nodes keys values Ascendingly.
+        /// </summary>
+        LeftToRight,
+
+        /// <summary>
+        /// Moves through tree nodes keys values Dscendingly.
+        /// </summary>
+        RightToLeft,
+        
+    }
+}
